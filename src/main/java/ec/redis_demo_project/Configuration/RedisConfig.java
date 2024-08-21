a79315af12c1e66e0915d66a0ad194ed086e616e
Ezgi Cinan <ezgi.cinan@marun.edu.tr>
2024-08-21 03:33:08 +0300

    Redis Cache configurations were added.

--- src/main/java/ec/redis_demo_project/Configuration/RedisConfig.java
+++ src/main/java/ec/redis_demo_project/Configuration/RedisConfig.java
@@ -1,15 +1,26 @@
 package ec.redis_demo_project.Configuration;
 
 import ec.redis_demo_project.SpringProperties.SpringDataProperties;
+import ec.redis_demo_project.util.CacheNames;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
+@EnableCaching
 @Log4j2
 public class RedisConfig {
 
@@ -18,6 +29,24 @@ public class RedisConfig {
     @Autowired
     public RedisConfig(SpringDataProperties springDataProperties) {
         this.springDataProperties = springDataProperties;
+    }
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration
+                .defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+    }
+
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+        return builder -> builder
+                .withCacheConfiguration(CacheNames.RECIPE_TITLES.cacheName,
+                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(60)));
     }
 
     @Bean
@@ -34,6 +63,9 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new JdkSerializationRedisSerializer());
+        template.setValueSerializer(new JdkSerializationRedisSerializer());
         return template;
     }
 }
